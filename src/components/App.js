1d869f961c305e1c349d9265a6c7a9b5153f01a5
Soumya Ranjan Nayak <soumyaranjannayak0140@gmail.com>
2023-09-18 21:50:50 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,35 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+ const [showModel, setShowModel] = useState(false);
+
+  function toggleModel() {
+    setShowModel(!showModel);
+  }
+
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div className="App">
+      <div className="parent">
+        <h1>Parent Component</h1>
+        <div className="child">
+          <h2>Child Component</h2>
+
+          <button onClick={toggleModel}>Show Model</button>
+          
+          {showModel && (
+            <div className="model">
+              <h2>Model Content</h2>
+              <p>This is a model content</p>
+              <button onClick={toggleModel}>X</button>
+            </div>
+          )}
+        </div>
+      </div>
     </div>
-  )
+  );
+
+
+  
 }
 
 export default App
